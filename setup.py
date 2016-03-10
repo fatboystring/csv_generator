fa42e15e18d276a85f65a1310a26f2325a30a8f7
Dan Stringer <daniel.stringer@omni-digital.co.uk>
2016-03-10 08:18:05 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-
+from __future__ import unicode_literals
 from setuptools import setup, find_packages
 
-setup(name='csv_generator',
+setup(
+      name='csv_generator',
       version='0.1',
       description='Configurable CSV Generator for Django',
       author='Dan Stringer',
@@ -10,4 +11,19 @@ setup(name='csv_generator',
       url='https://github.com/fatboystring/csv_generator/',
       packages=find_packages(),
       license='License :: Public Domain',
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+      ],
+      keywords='csv writer queryset django',
+      install_requires=[],
 )
