33abd89c365d2e4d4734e8065249cb5c69cbb18c
Dan Stringer <daniel.stringer@omni-digital.co.uk>
2016-03-21 19:57:10 +0000

    Updated docs and bumped version number

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals
+from csv_generator import __version__
 from setuptools import setup, find_packages
 
 setup(
       name='csv_generator',
-      version='0.1.0',
+      version=__version__,
       description='Configurable CSV Generator for Django',
       author='Dan Stringer',
       author_email='dan.stringer1983@googlemail.com',
