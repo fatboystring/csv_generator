f134f9757e05c8e21b04fdff6271e964520aa9bb
Dan Stringer <daniel.stringer@omni-digital.co.uk>
2016-05-16 20:16:27 +0100

    Add another attr for test model 2 when testing@

--- app/settings_develop.py
+++ app/settings_develop.py
@@ -2,5 +2,6 @@ from app.settings import *
 
 
 CSV_GENERATOR_AVAILABLE_ATTRIBUTES = {
-    'tests.testmodel': {'test_attr': 'Test Attribute'}
+    'tests.testmodel': {'test_attr': 'Test Attribute'},
+    'tests.testmodel2': {'test_attr': 'Test Attribute 2'}
 }
